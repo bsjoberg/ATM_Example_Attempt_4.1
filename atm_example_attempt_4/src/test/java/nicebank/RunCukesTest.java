ba37c3ebce24d13b79d2979bbf1479cad26f8ee2
bsjoberg <brian_sjoberg@yahoo.com>
2019-04-28 16:24:56 -0400

    Added run cukes test.

---
+++ atm_example_attempt_4/src/test/java/nicebank/RunCukesTest.java
@@ -0,0 +1,17 @@
+package nicebank;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty", "html:out" }, 
+				features = "src/test/resources", 
+				glue = { "nicebank"}, 
+				snippets = SnippetType.CAMELCASE)
+
+public class RunCukesTest {
+
+}

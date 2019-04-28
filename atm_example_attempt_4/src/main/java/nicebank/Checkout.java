78d6d96f247125972d69b030aac48531c09fcd2b
bsjoberg <brian_sjoberg@yahoo.com>
2019-04-28 16:50:19 -0400

    Stubbed out Checkout class and add method.

---
+++ atm_example_attempt_4/src/main/java/nicebank/Checkout.java
@@ -0,0 +1,10 @@
+package nicebank;
+
+public class Checkout {
+
+	public void add(Integer itemCount, int bananaPrice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- atm_example_attempt_4/src/test/java/nicebank/CheckoutSteps.java
+++ atm_example_attempt_4/src/test/java/nicebank/CheckoutSteps.java
@@ -5,15 +5,17 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class CheckoutSteps {
+	private int bananaPrice;
+
 	@Given("the price of a {string} is {int}c")
-	public void thePriceOfAIsC(String name, Integer price) {
-	    int bananaPrice = price;
+	public void thePriceOfAIsC(String name, int price) {
+	    bananaPrice = price;
 	}
 
 	@When("I checkout {int} {string}")
-	public void iCheckout(Integer itemCount, String itemName) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	public void iCheckout(Integer itemCount, int itemName) {
+	    Checkout checkout = new Checkout();
+	    checkout.add(itemCount, bananaPrice);
 	}
 
 	@Then("the total price should be {int}c")

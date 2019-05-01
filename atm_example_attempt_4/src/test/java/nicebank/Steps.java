ec765722998d20e85795af69d37a982ba90c76e4
bsjoberg <brian_sjoberg@yahoo.com>
2019-05-01 08:55:05 -0400

    Given step is passing.

--- atm_example_attempt_4/src/test/java/nicebank/Steps.java
+++ atm_example_attempt_4/src/test/java/nicebank/Steps.java
@@ -1,15 +1,31 @@
 package nicebank;
 
+import org.junit.Assert;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Steps {
 
+	class Account {
+		private Integer balance = 0;
+		
+		public void deposit(Integer amount) {
+			balance += amount;
+		}
+
+		public Integer getBalance() {
+			return balance;
+		}
+	}
+	
 	@Given("I have deposited ${int} in my account")
-	public void iHaveDeposited$InMyAccount(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
+	public void iHaveDeposited$InMyAccount(Integer amount) {
+	    Account myAccount = new Account();
+	    myAccount.deposit(amount);
+	    
+	    Assert.assertEquals("Incorrect Account balance", amount, myAccount.getBalance());
 	}
 
 	@When("I request ${int}")

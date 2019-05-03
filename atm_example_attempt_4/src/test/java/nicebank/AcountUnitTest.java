1250b133a6ec50d53a3b3e49121686c56c90c7ab
bsjoberg <brian_sjoberg@yahoo.com>
2019-05-03 08:44:49 -0400

    Unit test for withdraw is passing. Not checking for insufficient funds at the Account level though.

--- atm_example_attempt_4/src/test/java/nicebank/AcountUnitTest.java
+++ atm_example_attempt_4/src/test/java/nicebank/AcountUnitTest.java
@@ -28,4 +28,12 @@ public class AcountUnitTest {
 	public void test_should_not_be_able_to_add_a_negative_amount() {
 		
 	}
+	
+	@Test
+	public void test_should_be_able_to_withdraw_when_sufficient_funds_available() {
+		account.deposit(new Money(200,0));
+		account.withdraw(100);
+		
+		Assert.assertEquals("Incorrect Balance", new Money(100,0), account.getBalance());
+	}
 }

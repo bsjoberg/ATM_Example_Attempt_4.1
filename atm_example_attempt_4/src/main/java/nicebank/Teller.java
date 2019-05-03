13514415a10299b688b28d9cbac69c8a6d6b8438
bsjoberg <brian_sjoberg@yahoo.com>
2019-05-03 08:08:40 -0400

    Handling insufficient funds.

--- atm_example_attempt_4/src/main/java/nicebank/Teller.java
+++ atm_example_attempt_4/src/main/java/nicebank/Teller.java
@@ -3,13 +3,22 @@ package nicebank;
 
 public class Teller {
 	private CashSlot cashSlot;
+	boolean sufficientFunds = false;
 	
 	public Teller (CashSlot cashSlot) {
 		this.cashSlot = cashSlot;
 	}
 	
-	public void withdrawFrom(Account account, int dollars) {
-		cashSlot.dispense(dollars);
+	public boolean withdrawFrom(Account account, int dollars) {
+		if (account.getBalance().dollars() >= dollars) {
+			sufficientFunds = true;
+			cashSlot.dispense(dollars);
+		}
+		else {
+			sufficientFunds = false;
+			cashSlot.dispense(0);
+		}
+		return sufficientFunds;
 	}
 	
 	
